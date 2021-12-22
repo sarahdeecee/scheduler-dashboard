88d23bf2becabdba7dc4c4912fec46e64c5f9adb
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2021-12-22 15:46:25 -0500

    add changePanel function to set focus to a panel

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -28,18 +28,33 @@ const data = [
 
 class Dashboard extends Component {
   state = { 
-    loading: false
-   };
+    loading: false,
+    focused: null
+  };
+
+  changeFocus = function(id) {
+    this.setState({focused: id});
+  };
+
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+     });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    const panels = data.map(panel => (
-      <Panel key={panel.id} id={panel.id} label={panel.label} value={panel.value} />
-    ))
+    const panels = data
+      .filter(panel => this.state.focused === null || this.state.focused === panel.id)
+      .map(panel => (
+        <Panel
+          key={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={e => this.changeFocus(panel.id)}
+        />
+      ));
 
     return <main className={dashboardClasses}>{panels}</main>;
   }
